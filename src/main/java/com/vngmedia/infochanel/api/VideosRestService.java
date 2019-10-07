66a46097090e83961a3946b5aaa6aae2fccdba4e
huytranITO <you@example.com>
2019-10-07 17:41:36 +0700

    main video

---
+++ src/main/java/com/vngmedia/infochanel/api/VideosRestService.java
@@ -0,0 +1,10 @@
+package com.vngmedia.infochanel.api;
+
+import org.apache.log4j.Logger;
+
+
+public class VideosRestService {
+	
+	private static final Logger logger = Logger.getLogger(VideosRestService.class);
+
+}
